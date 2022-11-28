53c05cd258ac0dd3a6794f99265af61467bae3eb
MatteoPellas <pellas.matteo@iisgalvanimi.edu.it>
2022-11-28 13:42:41 +0000

    fine preparazione

---
+++ reddit/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles = new Array<string>();
+  addArticle(spamText: HTMLInputElement, num: HTMLInputElement): boolean {
+    console.log(`spamText: ${spamText.value} and spamNum: ${num.value}`);
+    let cont = Number(num.value); 
+    for(let i:number =0; i< cont; i++)
+ {
+  this.articles.push(spamText.value);
+ }
+ console.log(this.articles)
+
+
+  return false;
+  }
+
+  
+}
---
+++ reddit/src/app/visual/visual.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visual',
+  templateUrl: './visual.component.html',
+  styleUrls: ['./visual.component.css']
+})
+
+export class VisualComponent {
+  @Input() name : string = "";
+
+  constructor() {
+  }
+
+}
